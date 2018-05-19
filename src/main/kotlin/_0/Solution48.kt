01b9be6946098039a6c5ca44de2114695a158c49
Igor Perikov <perikov.igor@gmail.com>
2018-05-19 15:28:42 +0300

    #48

---
+++ src/main/kotlin/_0/Solution48.kt
@@ -0,0 +1,26 @@
+package _0
+
+class Solution48 {
+    fun rotate(matrix: Array<IntArray>): Unit {
+        for (i in 0 until matrix.size / 2) {
+            for (j in i..matrix.size - i - 2) {
+                var valueToMove = matrix[i][j]
+                var newI = i
+                var newJ = j
+                for (a in 1..4) {
+                    val newCoordinates = whereToMove(matrix, newI, newJ)
+                    newI = newCoordinates.first
+                    newJ = newCoordinates.second
+                    val removedValue: Int = matrix[newI][newJ]
+                    matrix[newI][newJ] = valueToMove
+                    valueToMove = removedValue
+                    println("$i$j next we're moving $valueToMove")
+                }
+            }
+        }
+    }
+
+    private fun whereToMove(matrix: Array<IntArray>, i: Int, j: Int): Pair<Int, Int> {
+        return Pair(j, matrix.size - i - 1)
+    }
+}
