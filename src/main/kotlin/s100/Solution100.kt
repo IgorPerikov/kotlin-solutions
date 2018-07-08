ef9192f897c379b44c36610070af536cb20299a4
Igor Perikov <perikov.igor@gmail.com>
2018-07-08 21:30:47 +0300

    #100

---
+++ src/main/kotlin/s100/Solution100.kt
@@ -0,0 +1,19 @@
+package s100
+
+import other.TreeNode
+
+class Solution100 {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        return if (p == null && q != null) {
+            false
+        } else if (p != null && q == null) {
+            false
+        } else if (p == null && q == null) {
+            true
+        } else if (p?.`val` != q?.`val`) {
+            false
+        } else {
+            isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        }
+    }
+}
